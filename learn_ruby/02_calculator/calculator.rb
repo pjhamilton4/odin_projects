cc5058501a4fd9fde09165e1ed5912d58d6aaf26
Philip Hamilton <pjhamilton4@gmail.com>
2019-12-10 00:51:03 -0500

    Finished 03

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+    return num1 + num2
+end
+
+def subtract(num1, num2)
+    return num1 - num2
+end
+
+def sum(nums)
+    sum = 0
+    for num in nums do
+        sum += num
+    end
+    return sum
+end
