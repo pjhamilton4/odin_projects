e87e1c8cacbe9d6445ac5d71fcb4abbac4fe5038
Philip Hamilton <pjhamilton4@gmail.com>
2019-12-10 22:59:34 -0500

    Finished 05 - Book Titles

--- learn_ruby/05_book_titles/book.rb
+++ learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def initialize()
+        @title = title
+    end
+
+    def title=(new_title)
+        @title = titleize(new_title)
+    end
+
+    def titleize(input)
+        no_cap = ["and", "of", "the", "over", "to", "the", "a", "but", "in", "an"]
+        capped = input.split(" ").map.with_index {
+            |word, index| 
+            if index == 0
+                word.capitalize
+            else
+                no_cap.include?(word) ? word : word.capitalize
+            end
+        }.join(' ')
+    end
+
 end
+
+@book = Book.new
